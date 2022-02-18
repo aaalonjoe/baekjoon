2835af72e1d281d623f17835473d54b2272f79c5
YOUNGMIN JO <aalonjoe@naver.com>
2022-02-18 10:03:32 +0900

    [22.02.18] 수정

--- 4. 1차원 배열/1. 최소,최대.js
+++ 4. 1차원 배열/1. 최소,최대.js
@@ -1,4 +1,6 @@
 
 let arr = [5, [20, 10, 35, 30, 7]];
+let arr2 = [5,4,3,2,1];
 
-console.log(Math.max(arr[1]), Math.min(arr[1]));
+console.log(Math.max(...arr[1]), Math.min(...arr[1]));
+console.log(Math.min(...arr2));
